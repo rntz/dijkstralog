108c772b4c401e439ca0bf6f559a4bb6ca87f20d
Michael Arntzenius <daekharel@gmail.com>
2025-04-30 23:07:57 -0700

    refactor Leapfrog again and rename done() to empty()

--- src/lftj.rs
+++ src/lftj.rs
@@ -1,18 +1,16 @@
 // ---------- SEEKABLE ITERATORS ----------
 pub trait Seek: Iterator {
-    fn done(&self) -> bool;
-    // precondition for key() and advance(): !self.done()
+    fn empty(&self) -> bool;
+    // precondition for key() and advance(): !self.empty()
     fn key(&self) -> Self::Item;
     fn advance(&mut self);
-    // seek() does not require !self.done(). Note that if seek() returns
-    // Some(item), then next() will return Some(item) again; we DO NOT advance
-    // past the item we return.
-    fn seek(&mut self, key: Self::Item) -> Option<Self::Item>;
+    // seek() does not require !self.empty().
+    fn seek(&mut self, key: Self::Item);
 }
 
 // an implementation of Iterator::next() in terms of Seek::{done,key,advance}.
 fn seek_next<T: Seek>(this: &mut T) -> Option<T::Item> {
-    if this.done() { return None }
+    if this.empty() { return None }
     let elem = this.key();
     this.advance();
     return Some(elem);
@@ -20,10 +18,10 @@ fn seek_next<T: Seek>(this: &mut T) -> Option<T::Item> {
 
 // ensure (&mut dyn Seek) implements Seek
 impl<S: Seek + ?Sized> Seek for &mut S {
-    #[inline] fn done(&self) -> bool { (**self).done() }
+    #[inline] fn empty(&self) -> bool { (**self).empty() }
     #[inline] fn key(&self) -> S::Item { (**self).key() }
     #[inline] fn advance(&mut self) { (**self).advance() }
-    #[inline] fn seek(&mut self, key: S::Item) -> Option<S::Item> { (**self).seek(key) }
+    #[inline] fn seek(&mut self, key: S::Item) { (**self).seek(key) }
 }
 
 
@@ -47,39 +45,49 @@ impl<'a, T: Ord> Iterator for SliceSeek<'a, T> {
 }
 
 impl<'a, T: Ord> Seek for SliceSeek<'a, T> {
-    fn done(&self) -> bool { self.posn >= self.elems.len() }
+    fn empty(&self) -> bool { self.posn >= self.elems.len() }
 
     fn key(&self) -> &'a T {
-        debug_assert!(!self.done());
+        debug_assert!(!self.empty());
         &self.elems[self.posn]
     }
 
     fn advance(&mut self) {
-        debug_assert!(!self.done());
+        debug_assert!(!self.empty());
         self.posn += 1;
     }
 
-    fn seek(&mut self, key: &T) -> Option<&'a T> {
+    fn seek(&mut self, key: &T) {
         debug_assert!(self.posn <= self.elems.len());
         self.posn += self.elems[self.posn..].partition_point(|x| x < key);
         debug_assert!(self.posn <= self.elems.len());
-        if self.posn == self.elems.len() { None }
-        else {
-            let elem = &self.elems[self.posn];
-            debug_assert!(key <= elem);
-            Some(elem)
-        }
+        debug_assert!(self.posn == self.elems.len() || key <= &self.elems[self.posn]);
     }
 }
 
 
 // ---------- LEAPFROG INTERSECTION ----------
-pub enum Leapfrog<Iter> {
-    Stop,
-    Go { index: usize, iters: Vec<Iter> },
-}
+pub struct Leapfrog<Iter>(Option<(usize, Vec<Iter>)>);
 
-use Leapfrog::{Stop,Go};
+fn leapfrog_search<I: Seek>(mut index: usize, mut iters: Vec<I>) -> Option<(usize, Vec<I>)>
+where I::Item: Eq
+{
+    let n = iters.len();
+    let mut hi = iters[if index>0 {index-1} else {n-1}].key();
+    loop {
+        let iter = &mut iters[index];
+        if hi == iter.key() { // all iters at same key
+            return Some((index, iters));
+        }
+        // Hm, if seek() returned an Option<K> this would work fine? but
+        // we'd have to get the same value again when we called next(); we
+        // don't want to advance _past_ the element.
+        iter.seek(hi);
+        if iter.empty() { return None; }
+        hi = iter.key();
+        index = (index + 1) % n;
+    }
+}
 
 impl<I: Seek> Leapfrog<I> where I::Item: Ord {
     // shouldn't I be able to implement this for any IntoIter thing?
@@ -87,49 +95,10 @@ impl<I: Seek> Leapfrog<I> where I::Item: Ord {
         // Intersecting nothing would produce "universal" relation, which I
         // can't represent efficiently.
         assert!(0 < iters.len());
-
         // If any iterator is already done, so are we.
-        if iters.iter().any(|x| x.done()) { return Stop }
-
+        if iters.iter().any(|x| x.empty()) { return Leapfrog(None) }
         iters.sort_by_key(|it| it.key());
-        let mut lf = Go { index: 0, iters };
-        lf.search();
-        lf
-    }
-
-    fn unwrap(&self) -> (usize, &Vec<I>) {
-        match self {
-            Stop => panic!("Leapfrog::look() called but iterator is done"),
-            Go { index, iters } => (*index, iters),
-        }
-    }
-
-    fn take(&mut self) -> (usize, Vec<I>) {
-        match std::mem::replace(self, Stop) {
-            Stop => panic!("Leapfrog::take() called but iterator is done"),
-            Go { index, iters } => (index, iters)
-        }
-    }
-
-    fn search(&mut self) -> Option<I::Item> {
-        let (mut index, mut iters) = self.take();
-        // rust has no modular arithmetic primitive, ugh.
-        let n = iters.len();
-        let mut hi = iters[if index>0 {index-1} else {n-1}].key();
-        loop {
-            let iter = &mut iters[index];
-            if hi == iter.key() { // all iters at same key
-                *self = Go { index, iters };
-                return Some(hi);
-            }
-            // Hm, if seek() returned an Option<K> this would work fine? but
-            // we'd have to get the same value again when we called next(); we
-            // don't want to advance _past_ the element.
-            iter.seek(hi);
-            if iter.done() { *self = Stop; return None; }
-            hi = iter.key();
-            index = (index + 1) % n;
-        }
+        return Leapfrog(leapfrog_search(0, iters))
     }
 }
 
@@ -139,41 +108,28 @@ impl<S: Seek> Iterator for Leapfrog<S> where S::Item: Ord {
 }
 
 impl<S: Seek> Seek for Leapfrog<S> where S::Item: Ord {
-    #[inline]
-    fn done(&self) -> bool {
-        match self { Stop => true, Go {..} => false }
-    }
+    fn empty(&self) -> bool { self.0.is_none() }
 
     fn key(&self) -> S::Item {
-        debug_assert!(!self.done());
-        let (index, iters) = self.unwrap();
-        iters[index].key()
+        debug_assert!(!self.empty());
+        let (index, iters) = self.0.as_ref().unwrap();
+        iters[*index].key()
     }
 
     fn advance(&mut self) {
-        debug_assert!(!self.done());
-        let (mut index, mut iters) = self.take();
+        debug_assert!(!self.empty());
+        let (index, mut iters) = self.0.take().unwrap();
         let iter = &mut iters[index];
         iter.advance();
-        if iter.done() {
-            *self = Stop;
-        } else {
-            index = (index+1) % iters.len();
-            *self = Go { iters, index };
-            self.search();
+        self.0 = if iter.empty() { None } else {
+            leapfrog_search((index+1) % iters.len(), iters)
         }
     }
 
-    fn seek(&mut self, key: S::Item) -> Option<S::Item> {
-        match self {
-            Stop => None,
-            Go { index, iters } => match iters[*index].seek(key) {
-                None => { *self = Stop; None }
-                Some(_) => {
-                    *index = (*index + 1) % iters.len();
-                    self.search()
-                }
-            }
+    fn seek(&mut self, key: S::Item) {
+        if let Some ((index, mut iters)) = self.0.take() {
+            iters[index].seek(key);
+            self.0 = leapfrog_search((index + 1) % iters.len(), iters);
         }
     }
 }
