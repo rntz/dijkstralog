1ecdc54679e15bbdf3c8aba661ffc8c5c0c26c8e
Michael Arntzenius <daekharel@gmail.com>
2025-05-19 23:25:49 -0700

    more work on alltogethernow

--- iters_alltogethernow.hs
+++ iters_alltogethernow.hs
@@ -3,13 +3,6 @@ import Debug.Trace (trace)
 import Data.Ord (comparing)
 import Data.List (sortBy)
 
--- Applicative without pure. A "semimonoidal functor".
--- In our case, this is actually the Zip typeclass from semialign!
-class Functor f => Apply f where
-  map2 :: (a -> b -> c) -> f a -> f b -> f c
-  pair :: f a -> f b -> f (a,b)
-  pair = map2 (,)
-
 -- A lower bound in a totally ordered key-space k; corresponds to some part of an
 -- ordered sequence we can seek forward to.
 data Bound k = Init | Atleast !k | Greater !k | Done deriving (Show, Eq)
@@ -37,14 +30,17 @@ data Position k v = Found !k v
                   | Bound !(Bound k)
                     deriving (Show, Eq, Functor)
 
-key :: Position k v -> Bound k
-key (Found k _) = Atleast k
-key (Bound k) = k
+atleast :: Position k v -> Bound k
+atleast (Found k _) = Atleast k
+atleast (Bound k)   = k
 
 data Iter k v = Iter
   { posn :: !(Position k v)
   , seek :: Bound k -> Iter k v
   } deriving Functor
+
+bound :: Iter k v -> Bound k
+bound = atleast . posn
 
 emptyIter :: Iter k v
 emptyIter = Iter (Bound Done) (const emptyIter)
@@ -93,19 +89,28 @@ fromList = fromSorted . sortBy (comparing fst)
 traceFromList name = traceFromSorted name . sortBy (comparing fst)
 
 
+-- Any function (k -> Maybe v) can become an unproductive Iter.
+fromFunction :: (k -> Maybe v) -> Iter k v
+fromFunction f = seek Init
+  where seek k = Iter (at k) seek
+        at (Atleast k) = case f k of
+                           Just v  -> Found k v
+                           Nothing -> Bound (Greater k)
+        at p = Bound p
+
+always :: v -> Iter k v
+always x = fromFunction (\_ -> Just x)
+
+
 -- Inner joins, ie generalized intersection.
-
--- A value-aware maximum: we find the maximum position, and combine the values
--- if present.
-instance Ord k => Apply (Position k) where
-  map2 f (Found k1 v1) (Found k2 v2) | k1 == k2 = Found k1 (f v1 v2)
-  map2 f x y = Bound (key x `max` key y)
-
-instance Ord k => Apply (Iter k) where
-  map2 f s t = Iter (map2 f (posn s) (posn t)) seek'
-    where seek' k = map2 f s' t'
+instance Ord k => Applicative (Iter k) where
+  pure = always
+  liftA2 f s t = Iter posn' seek'
+    where posn' | Found k x <- posn s, Found k' y <- posn t, k == k' = Found k (f x y)
+                | otherwise = Bound (bound s `max` bound t)
+          seek' k = liftA2 f s' t'
             where s' = seek s k
-                  t' = seek t $ key $ posn s' -- the leapfrog trick
+                  t' = seek t $ bound s' -- the leapfrog trick
           -- -- The simple implementation without sideways information passing.
           -- seek' k = map2 f (seek s k) (seek t k)
 
@@ -122,36 +127,35 @@ class Functor f => OuterJoin f where
 -- A value-aware minimum: we find the minimum position, and apply the
 -- appropriate function (l, r, b, or nothing) to the values present.
 instance Ord k => OuterJoin (Position k) where
-  outerJoin l r b p q =
-    case key p `compare` key q of
-      LT -> l <$> p
-      GT -> r <$> q
-      EQ -> case (p, q) of
-              (Found k x, Found _ y) -> Found k (b x y)
-              -- If they're equal but one isn't done finding a value yet, we
-              -- have to wait until it does.
-              (Bound pos, _) -> Bound pos
-              (_, Bound pos) -> Bound pos
+  -- Concise (but inefficient?) implementation.
+  outerJoin l r b p q = case (atleast p `compare` atleast q, p, q) of
+      (LT, Found k x, _)         -> Found k (l x)
+      (GT, _,         Found k y) -> Found k (r y)
+      (EQ, Found k x, Found _ y) -> Found k (b x y)
+      _                          -> Bound (atleast p `min` atleast q)
+
+  -- -- Eliminating the repeated comparison.
+  -- outerJoin l r b p q = case (atleast p `compare` atleast q, p, q) of
+  --     (LT, Found k x, _)         -> Found k (l x)
+  --     (GT, _, Found k y)         -> Found k (r y)
+  --     (EQ, Found k x, Found _ y) -> Found k (b x y)
+  --     (GT, _, _)                 -> Bound (atleast q)
+  --     _                          -> Bound (atleast p)
+
+  -- -- Most explicit.
+  -- outerJoin l r b p q = case atleast p `compare` atleast q of
+  --     LT -> l <$> p
+  --     GT -> r <$> q
+  --     EQ -> case (p, q) of
+  --             -- If they're equal, we must wait until either both find a value
+  --             -- or one exceeds the other.
+  --             (Bound pos, _)         -> Bound pos
+  --             (_, Bound pos)         -> Bound pos
+  --             (Found k x, Found _ y) -> Found k (b x y)
 
 instance Ord k => OuterJoin (Iter k) where
   outerJoin l r b s t = Iter (outerJoin l r b (posn s) (posn t))
                              (\k -> outerJoin l r b (seek s k) (seek t k))
-
-
--- Converting a function "k -> Maybe v" into an unproductive Iter.
-fromFunction :: (k -> Maybe v) -> Iter k v
-fromFunction f = seek Init
-  where seek k = Iter (at k) seek
-        at (Atleast k) = case f k of
-                           Just v  -> Found k v
-                           Nothing -> Bound (Greater k)
-        at p = Bound p
-
-filterKey :: Ord k => (k -> Bool) -> Iter k v -> Iter k v
-filterKey test s = map2 (\x y -> x) s (fromFunction (\k -> if test k then Just () else Nothing))
-
-always :: a -> Iter k a
-always x = fromFunction (\_ -> Just x)
 
 
 -- Semiring magic.
@@ -172,8 +176,8 @@ instance (Ord k, Additive a) => Additive (Iter k a) where
   add = outerJoin id id add
 
 instance (Ord k, Multiply a) => Multiply (Iter k a) where
-  one = always one -- NB. unproductive
-  mul = map2 mul
+  one = pure one -- NB. unproductive
+  mul = liftA2 mul
 
 instance Additive Bool where zero = False; add = (||)
 instance Multiply Bool where one  = True;  mul = (&&)
@@ -197,14 +201,14 @@ ms1, ms2 :: Iter Int String
 ms1 = traceFromList "A" list1
 ms2 = traceFromList "B" list2
 
-m = pair ms1 ms2
+m = liftA2 (,) ms1 ms2
 
 xs = traceFromList "x" [(x,x) | x <- [1,3 .. 100]]
 ys = traceFromList "y" [(y,y) | y <- [2,4 .. 100]]
 zs = traceFromList "z" [(z,z) | z <- [1,100]]
 
 mxyz :: Iter Int ((Int, Int), Int)
-mxyz = pair (pair xs ys) zs
+mxyz = liftA2 (,) (liftA2 (,) xs ys) zs
 
 -- avoids interleaving of `trace` output with printing of value at REPL
 drain x = length xs `seq` xs where xs = toSorted x
---
+++ iters_alltogethernow_minimal.hs
@@ -0,0 +1,95 @@
+-- A lower bound in a totally ordered key-space k; corresponds to some part of an
+-- ordered sequence we can seek forward to.
+data Bound k = Init | Atleast !k | Greater !k | Done deriving (Show, Eq)
+instance Ord k => Ord (Bound k) where
+  Init      <= _         = True
+  _         <= Done      = True
+  _         <= Init      = False
+  Done      <= _         = False
+  Atleast x <= Atleast y = x <= y
+  Atleast x <= Greater y = x <= y
+  Greater x <= Atleast y = x <  y -- <== NB. the odd one out!
+  Greater x <= Greater y = x <= y
+
+-- An iterator has either found a particular key-value pair, or knows a lower
+-- bound on future keys.
+data Position k v = Found !k v | Bound !(Bound k) deriving (Show, Eq, Functor)
+atleast :: Position k v -> Bound k
+atleast (Found k _) = Atleast k
+atleast (Bound p)   = p
+
+data Iter k v = Iter { posn :: !(Position k v), seek :: Bound k -> Iter k v } deriving Functor
+key :: Iter k v -> Bound k
+key = atleast . posn
+
+-- Any key-value function can be an iterator - just not a productive one.
+fromFunction :: (k -> Maybe v) -> Iter k v
+fromFunction f = seek Init
+  where seek k = Iter (at k) seek
+        at (Atleast k) = maybe (Bound (Greater k)) (Found k) (f k)
+        at p           = Bound p
+
+-- Inner joins, ie generalized intersection. liftA2 is productive if either argument is.
+instance Ord k => Applicative (Iter k) where
+  pure x = fromFunction (\_ -> Just x)
+  liftA2 f s t = Iter posn' seek'
+    where posn' | Found k x <- posn s, Found k' y <- posn t, k == k' = Found k (f x y)
+                | otherwise                                          = Bound (key s `max` key t)
+          seek' k = liftA2 f s' t'
+            where s' = seek s k
+                  t' = seek t $ key s' -- the leapfrog trick
+          -- -- Simpler implementation with same asymptotics:
+          -- seek' k = liftA2 f (seek s k) (seek t k)
+
+-- Outer joins, ie generalized union.
+class Functor f => OuterJoin f where
+  outerJoin :: (a -> c) -> (b -> c) -> (a -> b -> c) -> f a -> f b -> f c
+
+instance Ord k => OuterJoin (Position k) where
+  outerJoin l r b p q = case (atleast p `compare` atleast q, p, q) of
+      (LT, Found k x, _)         -> Found k (l x)
+      (GT, _,         Found k y) -> Found k (r y)
+      (EQ, Found k x, Found _ y) -> Found k (b x y)
+      _                          -> Bound (atleast p `min` atleast q)
+
+instance Ord k => OuterJoin (Iter k) where
+  -- outerJoin is productive if both arguments are.
+  outerJoin l r b s t = Iter (outerJoin l r b (posn s) (posn t))
+                             (\k -> outerJoin l r b (seek s k) (seek t k))
+
+
+
+-- NB. We can't actually seek efficiently in a Haskell list.
+fromSorted :: Ord k => [(k,v)] -> Iter k v
+fromSorted []          = emptyIter
+fromSorted l@((k,v):_) = Iter (Found k v) seek
+  where seek target = fromSorted $ dropWhile (not . matches target . fst) l
+
+toSorted :: Iter k v -> [(k,v)]
+toSorted (Iter (Bound Done) _)   = []
+toSorted (Iter (Found k v) seek) = (k,v) : toSorted (seek (Greater k))
+toSorted (Iter (Bound k) seek)   = toSorted $ seek k
+
+emptyIter = Iter (Bound Done) (const emptyIter)
+
+matches :: Ord k => Bound k -> k -> Bool
+matches Init        _ = True
+matches Done        _ = False
+matches (Atleast x) y = x <= y
+matches (Greater x) y = x <  y
+
+list1 = [(1, "one"), (2, "two"), (3, "three"), (5, "five")]
+list2 = [(1, "a"), (3, "c"), (5, "e")]
+
+ms1, ms2 :: Iter Int String
+ms1 = fromSorted list1
+ms2 = fromSorted list2
+m = (,) <$> ms1 <*> ms2
+
+xs = fromSorted [(x,x) | x <- [1,3 .. 100]]
+ys = fromSorted [(y,y) | y <- [2,4 .. 100]]
+zs = fromSorted [(z,z) | z <- [1,100]]
+
+mxyz :: Iter Int ((Int, Int), Int)
+mxyz = (,) <$> ((,) <$> xs <*> ys) <*> zs
+
