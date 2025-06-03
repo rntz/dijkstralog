6586e02a3622e611cffb0b7d2d2e4eb9b6c9a6c9
Michael Arntzenius <daekharel@gmail.com>
2025-06-03 15:52:14 -0700

    examples in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code, unused_variables, unused_mut, unused_imports)]
+#![allow(dead_code, unused_imports, unused_variables, unused_mut)]
 
 // old code that we don't need but still want to compile
 mod lftj;
@@ -16,22 +16,91 @@ use iters::{
     Seek,
 };
 
-fn main() {
-    println!("hello world!");
-
+// EXAMPLE 1: TRIES
+fn example1() {
     let xs: &[(isize, &str)] = &[(1, "one"), (1, "wun"), (2, "two"), (2, "deux")];
-    let mut it = SliceRange::new(xs, |x| x.0);
 
+    // Iterate through the slices on the first component of the tuples.
+    let mut it = SliceRange::new(xs, |x| x.0);
     loop {
         let p = it.posn();
         println!();
         println!(" p: {p:?}");
         println!("it: {it:?}");
-
         match p {
             Know(Done) => break,
             Know(p) => it.seek(&p),
             Have(k, _) => it.seek(&Greater(k)),
         }
     }
+
+    // Dump it into a trie using nested SliceRange iteration.
+    println!();
+    let trie =
+        SliceRange::new(xs, |tuple| tuple.0)
+        .collect_with(|x, tuples| {
+            let vec =
+                SliceRange::new(tuples, |tuple| tuple.1)
+                .collect_with(|y, tuples| {
+                    assert!(tuples.len() == 1);
+                    *y
+                });
+            (*x, vec)
+        });
+    println!("trie: {trie:?}");
+}
+
+// EXAMPLE 2: TRIANGLE QUERY
+fn example2() {
+    let r: &[(&str, isize)] = &[("a", 1), ("a", 2), ("b", 1), ("b", 2)];
+    let s: &[(isize, &str)] = &[(1, "one"), (1, "wun"), (2, "deux"), (2, "two")];
+    let t: &[(&str, &str)]  = &[("a", "one"), ("b", "deux"), ("mary", "mary")];
+    assert!(r.is_sorted());
+    assert!(s.is_sorted());
+    assert!(t.is_sorted());
+
+    let mut r_ =
+        SliceRange::new(r, |x| x.0)
+        .map(|tuples| SliceRange::new(tuples, |x| x.1).map(|slice| 2));
+    let mut s_ =
+        SliceRange::new(s, |x| x.0)
+        .map(|tuples| SliceRange::new(tuples, |x| x.1).map(|slice| 3));
+    let mut t_ = SliceRange::new(t, |x| x.0)
+        .map(|tuples| SliceRange::new(tuples, |x| x.1).map(|slice| 5));
+
+    let rtrie = r_.clone().collect_with(|a, bit| {
+        (*a, bit.collect_with(|b, v| (*b,v)))
+    });
+    println!("rtrie: {rtrie:?}");
+
+    // Let's plan a triangle query!
+    // this involves many sacrifices to the borrow checking gods.
+    // I worry about what the closures look like...
+    // but I expect disassembly to produce far too much code to inspect by hand.
+    let triangle_it =
+        r_.join(t_)
+        .map(move |(r_a, t_a)| {
+            r_a.join(s_.clone())
+                .map(move |(r_ab, s_b)| {
+                    s_b.join(t_a.clone())
+                        .map(move |(s_bc, t_ac)| {
+                            r_ab * s_bc * t_ac
+                        })
+                })
+        });
+
+    let vs = triangle_it
+        .collect_with(|a, tuples| {
+            (*a, tuples.collect_with(|b, tuples| {
+                (*b, tuples.collect_with(|c, value| (*c,value)))
+            }))
+        });
+    println!("vs: {vs:?}");
+}
+
+fn main() {
+    println!("hello world!");
+    example1();
+    println!();
+    example2();
 }
