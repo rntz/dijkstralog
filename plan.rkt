a5757896e254fef8e80b5b735ca50f2b89d75845
Michael Arntzenius <daekharel@gmail.com>
2025-06-04 17:55:52 -0700

    work on plan.rkt

--- plan.rkt
+++ plan.rkt
@@ -7,10 +7,31 @@
   (unless test
     (error "ASSERTION FAILURE:" 'test)))
 
+(define (reverse-append revs tail)
+  (if (null? revs) tail
+      (reverse-append (cdr revs) (cons (car revs) tail))))
+(define-syntax-rule (for*/append (for-clause ...) body-or-break ... body)
+  (for*/fold ([reverse-results '()] #:result (reverse reverse-results))
+             (for-clause ...)
+    body-or-break ...
+    (reverse-append body reverse-results)))
+
+(define term? any/c)
+(define var? symbol?)
+(define atom? (cons/c symbol? (listof term?)))
+(define conjunctive-query? (listof atom?))
+(define query-plan? any/c) ;; TODO: contract for query plans
+(define decls? any/c)      ;; TODO: contract for decls
+(define var-order? (listof var?))
+
 (define atom-pred car)
 (define atom-args cdr)
 
-(define var? symbol?)
+(define (deduplicate seq)
+  (define visited (mutable-set))
+  (for/list ([x seq] #:when (not (set-member? visited x)))
+    (set-add! visited x)
+    x))
 
 ;; A notion of subsequence that allows terms to be duplicated in xs. We assume
 ;; terms are not duplicated in ys but do not check this. We can use a term from
@@ -31,11 +52,58 @@
            (loop (cons i acc) i       (cdr xs) ys)
            (loop acc          (+ 1 i) xs       (cdr ys)))])))
 
+;; Heuristically picks a variable order for a conjunctive query.
+;; Puts variables which are used in more atoms first.
+;; After that, vaguely tries to not reorder variables.
+(define/contract (heuristic-var-order query)
+  (-> conjunctive-query? var-order?)
+  (define counts (make-hash))
+  (define atom-orders
+    (for/list ([atom query])
+      (deduplicate (atom-args atom))))
+  (for* ([order atom-orders] [x order])
+    (hash-update! counts x add1 0))
+  (define levels (make-hash))
+  (for ([(x n) counts])
+    (hash-update! levels n (curry cons x) '()))
+  (for*/append ([n (sort (hash-keys levels) >)])
+    (define xs (hash-ref levels n))
+    ;; (inversions x) = how many times do other variables in this level appear
+    ;; before x? This is an imperfect heuristic for avoiding variable
+    ;; reordering.
+    (define (inversions x)
+      (for*/sum ([y xs]
+                 #:when (not (equal? x y))
+                 [order atom-orders]
+                 #:do [(define after-y (member y order))]
+                 #:when (and after-y (member x after-y)))
+        1))
+    (sort xs < #:key inversions #:cache-keys? #t)))
+
+(define/contract (compile-auto query)
+  (-> conjunctive-query? (values var-order? decls? query-plan?))
+  (define var-order (heuristic-var-order query))
+  (define-values (decls query-plan) (compile var-order query))
+  (values var-order decls query-plan))
+
+;; Compiles a single atom to a query plan, deriving the variable order from the
+;; atom. Doesn't need any additional decls. Used for planning sorts.
+(define/contract (compile-atom atom)
+  (-> atom? (values var-order? query-plan?))
+  (define vars-seen (mutable-set))
+  (define var-order (deduplicate (filter var? (atom-args atom))))
+  (define-values (decls plan) (compile var-order `(,atom)))
+  (assert! (null? decls))
+  (values var-order plan))
+
 ;; (compile var-order query) --> (values query-plan decls)
 ;; decls: a list of indices that we need for this query
-(define (compile var-order query)
+(define/contract (compile var-order query)
+  (-> var-order? conjunctive-query? (values decls? query-plan?))
   (assert! (= (length var-order) (set-count (list->set var-order))))
-  (assert! (for*/and ([atom query] [x (atom-args atom)] #:when (var? x))
+  (assert! (for*/and ([atom query]
+                      [x (atom-args atom)]
+                      #:when (var? x))
              (member x var-order)))
 
   ;; NOTE/TODO: we could get rid of constants and duplicated variables up front
@@ -45,11 +113,13 @@
   (define decls '())
   (define query-sorted
     (for/list ([atom query])
-      (define atom-vars (filter var? (atom-args atom)))
-      (if (subsequence atom-vars var-order) atom
+      ;; Only the first occurrence of a variable matters; the others can be
+      ;; lookups; so we deduplicate.
+      (define atom-var-order (deduplicate (filter var? (atom-args atom))))
+      (if (subsequence atom-var-order var-order) atom
           ;; Otherwise, need to re-sort!
           (let* ([new-pred (gensym (atom-pred atom))]
-                 [new-vars (filter (lambda (x) (member x atom-vars)) var-order)]
+                 [new-vars (filter (lambda (x) (member x atom-var-order)) var-order)]
                  [new-atom `(,new-pred ,@new-vars)]
                  ;; TODO: re-use indices via hash-consing re-sortings
                  [sort-decl `(sort ,new-atom ,atom)])
@@ -85,24 +155,10 @@
                  #:when (equal? var (car iter-args)))
         (set-mcdr! iter-state (cdr iter-args))
         iter))
-    (plan-push! `(,var <- join ,@join-iters))
+    (plan-push! `(,var <- ,@join-iters))
     (lookups!))
 
-  (values (reverse reverse-query-plan) decls))
-
-;; Compiles a single atom to a query plan, deriving the variable order from the
-;; atom. Doesn't need any additional decls. Used for planning sorts.
-(define (compile-atom atom)
-  (define vars-seen (mutable-set))
-  (define var-order
-    (for/list ([x (atom-args atom)]
-               #:when (var? x)
-               #:when (not (set-member? vars-seen x)))
-      (set-add! vars-seen x)
-      x))
-  (define-values (plan decls) (compile var-order `(,atom)))
-  (assert! (null? decls))
-  plan)
+  (values decls (reverse reverse-query-plan)))
 
 (module+ tests
   ;; triangle query with a lookup in T.
@@ -111,6 +167,10 @@
   (compile '(a b c) '((R a b) (S b c) (T c 17 a)))
   ;; make sure it looks up the second b in S.
   (compile '(a b c) '((R a b) (S b b) (T a 17 c)))
+
+  ;; BUG: generates an unnecessary index for the repeated b in S.
+  ;; this is because subsequence is the wrong check!
+  (compile '(a b c) '((R a b) (S b c b)))
 
   ;; make sure we can compile a sort body.
   (compile-atom '(T c 17 a))
