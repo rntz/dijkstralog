bd4efa57cb0e9203c7e98743b5cd7952a309a0c5
Michael Arntzenius <daekharel@gmail.com>
2025-06-16 21:35:35 -0700

    implement more LSM stuff

--- src/lsm.rs
+++ src/lsm.rs
@@ -1,13 +1,17 @@
 // Many things here are adapted from
 // https://github.com/frankmcsherry/blog/blob/master/posts/2025-06-03.md
 
-use crate::iter;
-use crate::iter::Seek;
+use crate::iter::*;
 
 // Needed for combining values when deduplicating (eg merging levels).
 pub trait Add {
     fn zero() -> Self;
     fn plus(self, other: Self) -> Self;
+}
+
+impl Add for () {
+    fn zero() -> () { () }
+    fn plus(self, _other: ()) -> () { () }
 }
 
 // NURSERY_MAX_SIZE is the maximum size the smallest LSM layer (the "nursery",
@@ -32,7 +36,16 @@ pub trait Add {
 // And that's how an LSM is grown!
 const NURSERY_MAX_SIZE: usize = 64;
 
+// The maximum # of levels we can have in a LSM. We need this because Seek outer
+// joins / unions are implemented using fixed-size arrays to avoid heap
+// allocation. We need unions to iterate over an LSM because an LSM is the union
+// of its layers. Currently this can be at most 32 because we use
+// Default::default() to construct our arrays, and Default is only implemented
+// for arrays up to size 32 in Rust's stdlib.
+pub const MAX_LEVELS: usize = 32;
+
 // A sorted, de-duplicated list.
+// TODO: rename this "Sorted"?
 #[derive(Clone)]
 pub struct Layer<A> {
     // DataToad uses <Fact as Columnar>::Container here instead of just
@@ -42,10 +55,9 @@ pub struct Layer<A> {
 
 impl<A> Default for Layer<A> { fn default() -> Self { Layer { elems: Vec::new() } } }
 
-impl<K: Copy + Ord, V: Copy> Layer<(K, V)> {
-    pub fn seek(&self) -> impl Seek<Key = K, Value = V> {
-        iter::tuples(self.elems.as_slice(), |&(k, _)| k).map(|&(_, v)| v)
-    }
+impl<A> Layer<A> {
+    pub fn as_slice(&self) -> &[A] { self.elems.as_slice() }
+    pub fn len(&self) -> usize { self.elems.len() }
 }
 
 impl<K, V> Layer<(K, V)>
@@ -79,21 +91,19 @@ where
 
     pub fn merge(&mut self, other: Layer<(K, V)>) {
         // We should merge two layers only when their sizes are within a power
-        // of 2 of each other, and always merge smaller `self` into larger
-        // `other` (not that it currently matters for performance given the way
-        // this is implemented).
-        debug_assert!(self.elems.len() <= other.elems.len());
+        // of 2 of each other.
+        debug_assert!(self.elems.len() <= 2 * other.elems.len());
         debug_assert!(other.elems.len() <= 2 * self.elems.len());
         debug_assert!(self.elems.is_sorted_by_key(|&(k,_)| k));
         debug_assert!(other.elems.is_sorted_by_key(|&(k,_)| k));
 
-        self.elems = iter::tuples(self.elems.as_slice(), |x| x.0)
-            .outer_join(iter::tuples(other.elems.as_slice(), |x| x.0))
+        self.elems = tuples(self.elems.as_slice(), |x| x.0)
+            .outer_join(tuples(other.elems.as_slice(), |x| x.0))
             .map(|outer| match outer {
                 //  ~*~=~*~  SEMIRING SEMANTICS  ~*~=~*~
-                iter::OuterPair::Both((_, x), (_, y)) => x.clone().plus(y.clone()),
-                iter::OuterPair::Left((_, x)) => x.clone(),
-                iter::OuterPair::Right((_, y)) => y.clone(),
+                OuterPair::Both((_, x), (_, y)) => x.clone().plus(y.clone()),
+                OuterPair::Left((_, x)) => x.clone(),
+                OuterPair::Right((_, y)) => y.clone(),
             })
             .iter()
             .collect();
@@ -152,17 +162,67 @@ where
 }
 
 // Log-structured merge "tree" of sorted lists.
-#[derive(Clone, Default)]
+#[derive(Clone)]
 pub struct LSM<A> {
     // A list of layers in order, smallest first.
     // No two layers are within a power of two in size.
     layers: Vec<Layer<A>>,
 }
 
+impl<A> Default for LSM<A> { fn default() -> Self { LSM { layers: Vec::new() } } }
+
+impl<'a, A: Copy + Ord> IntoIterator for &'a LSM<A> {
+    type Item = A;
+    // absurdly large type tiem.
+    type IntoIter = IterKeys<OuterArray<{ MAX_LEVELS }, Option<Elements<'a, A>>>>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.seek(|slice| elements(slice)).keys()
+    }
+}
+
+impl<A> LSM<A> {
+    pub fn layers(&self) -> impl Iterator<Item = &Layer<A>> {
+        self.layers.iter()
+    }
+}
+
+impl<A> LSM<A> where
+    A: Copy + Ord,
+{
+    // We might want to ensure our return type is also Clone; it's helpful to be
+    // able to clone Seekerators.
+    pub fn seek<'a, S, F>(
+        &'a self,                 // <========== IMPORTANT LIFETIME ANNOTATION
+        seek_slice: F,
+    ) -> OuterArray<{ MAX_LEVELS }, Option<S>> // annoying Option
+    where
+        S: Seek,
+        // Default: needed to construct OuterArray of values in join result tuples.
+        S::Value: Default,
+        F: Fn(&'a [A]) -> S, // <========== MATCHING LIFETIME ANNOTATION
+    {
+        assert!(self.layers.len() <= MAX_LEVELS);
+        let array: OuterArray<{ MAX_LEVELS }, _> =
+            self.layers
+            .iter()
+            // Some() is an annoying hack to make the iterator type implement
+            // Default. TODO: instead create an empty layer and manually
+            // initialize the rest of the OuterArray with iterators over it.
+            .map(|layer| Some(seek_slice(layer.elems.as_slice())))
+            .into();
+        return array
+    }
+
+}
+
 impl<K, V> LSM<(K, V)> where
     K: Copy + Ord,
-    V: Copy + Default + Add,    // the Default is needed by OuterArray
+    // V: Clone is needed for Layer::merge (currently).
+    // V: Default is needed for OuterArray::from.
+    V: Clone + Default + Add,
 {
+    pub fn new() -> Self { LSM { layers: Vec::new() } }
+
     // TODO: If we hand push() a layer that's bigger than some layers already
     // present, currently it will simply merge all these into itself until it
     // hits a big enough layer. This is... fine? The cost of constructing the
@@ -193,29 +253,33 @@ impl<K, V> LSM<(K, V)> where
         };
     }
 
-    pub fn seek(&self) -> impl Seek<Key=K, Value=V> {
-        // 4 billion tuples should be enough for anyone!
-        //
-        // TODO: bump size to 48 and manually initialize a 48-element OuterArray
-        // here instead of relying on From.
-        const N: usize = 32;
-        assert!(self.layers.len() < N);
-        let array: iter::OuterArray<N, _> =
-            self.layers
-            .iter()
-            // Some() is an annoying hack to make the iterator type implement
-            // Default. TODO: instead create an empty layer and manually
-            // initialize the rest of the OuterArray with iterators over it.
-            .map(|layer| Some(layer.seek()))
-            .into();
-        array.map(|vs| {
-            // Sum up all the elements we found.
-            let mut x: V = Add::zero();
-            for y in vs.as_slice() {
-                x = x.plus(*y);
-            }
-            x
-        })
-    }
+    // pub fn seek(&self) -> impl Seek<Key=K, Value=V> {
+    //     // 4 billion tuples should be enough for anyone!
+    //     //
+    //     // TODO: bump size to 48 and manually initialize a 48-element OuterArray
+    //     // here instead of relying on From.
+    //     const N: usize = 32;
+    //     assert!(self.layers.len() < N);
+    //     let array: OuterArray<N, _> =
+    //         self.layers
+    //         .iter()
+    //     // Some() is an annoying hack to make the iterator type implement
+    //     // Default. TODO: instead create an empty layer and manually
+    //     // initialize the rest of the OuterArray with iterators over it.
+    //         .map(|layer| Some(layer.seek()))
+    //         .into();
+    //     array.map(|vs| {
+    //         // Sum up all the elements we found.
+    //         let mut x: V = Add::zero();
+    //         for y in vs.as_slice() {
+    //             x = x.plus(*y);
+    //         }
+    //         x
+    //     })
+    // }
 
+    // 4 billion tuples should be enough for anyone!
+    //
+    // TODO: bump MAX_LEVELS to 48 and manually initialize a 48-element
+    // OuterArray in seek() instead of relying on From.
 }
