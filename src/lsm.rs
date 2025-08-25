f00952173094b83bc1d2d1f4b6aef604f33839a7
Michael Arntzenius <daekharel@gmail.com>
2025-08-25 13:53:34 +0200

    changes to lsm.rs, multiple merge strategies

--- src/lsm.rs
+++ src/lsm.rs
@@ -38,83 +38,120 @@ impl<A> Default for Layer<A> {
     fn default() -> Self { Layer { elems: Vec::new() } }
 }
 
-impl<A> Layer<A> {
-    pub fn as_slice(&self) -> &[A] { self.elems.as_slice() }
-}
-
 impl<A> std::ops::Deref for Layer<A> {
     type Target = [A];
-    fn deref(&self) -> &[A] { self.as_slice() }
+    fn deref(&self) -> &[A] { self.elems.as_slice() }
+}
+
+impl<A> Layer<A> {
+    pub fn new() -> Self { Layer { elems: Vec::new() } }
+
+    pub fn as_slice(&self) -> &[A] { self.elems.as_slice() }
+
+    pub fn iter(&self) -> impl Iterator<Item = &A> {
+        self.elems.iter()
+    }
 }
 
 impl<K: Copy + Ord, V> Layer<Pair<K, V>> {
-    pub fn new() -> Self { Layer { elems: Vec::new() } }
-
     pub fn from_sorted(elems: Vec<Pair<K, V>>) -> Self {
         debug_assert!(elems.is_sorted_by_key(|x| x.key));
         Layer { elems }
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = &Pair<K, V>> {
-        self.elems.iter()
+    pub fn merged(&self, other: &Layer<Pair<K, V>>) -> Self
+    where V: Add + Clone
+    {
+        // this is mostly copied from DataToad I think
+        println!(" merging {} with {} nondestructively", self.len(), other.len());
+        let mut results: Vec<Pair<K, V>> = Vec::with_capacity(self.elems.len() + other.elems.len());
+        let mut iter1 = self.elems.iter().peekable();
+        let mut iter2 = other.elems.iter().peekable();
+        use std::cmp::Ordering::*;
+        while let (Some(x), Some(y)) = (iter1.peek(), iter2.peek()) {
+            match x.key.cmp(&y.key) {
+                Less => { results.push(iter1.next().unwrap().clone()); }
+                Greater => { results.push(iter2.next().unwrap().clone()); }
+                Equal => {
+                    let mut x = iter1.next().unwrap().clone();
+                    let y = iter2.next().unwrap();
+                    x.value = x.value.plus(y.value.clone());
+                    results.push(x);
+                }
+            }
+        }
+        results.extend(iter1.cloned());
+        results.extend(iter2.cloned());
+        return Layer { elems: results }
     }
 
-    pub fn merge(mut self, other: Layer<Pair<K, V>>) -> Self
-    where V: Add + Clone
+    pub fn merge(self, other: Layer<Pair<K, V>>) -> Self
+    where V: Add
     {
         println!(" merging {} with {}", self.len(), other.len());
 
-        // TODO: Instead of using an outer join, implement this manually. Do I get a
-        // significant speedup? Then my outer join implementation sucks and I need to fix
-        // it.
-        //
         // I get a measurable, but not large, speedup from using this hand-written merge
-        // instead of outer_join. Transitive closure on first 150k edges in
+        // instead of merge_outer_join, below. Transitive closure on first 150k edges in
         // soc-LiveJournal1.txt goes from 24s (outer_join) to 21s (handwritten).
+        //
+        // TODO: Can outer_join be improved to match the performance of this manual
+        // implementation?
 
-        if true {              // handwritten
-            let mut results: Vec<Pair<K, V>> = Vec::with_capacity(self.elems.len() + other.elems.len());
-            let mut iter1 = self.elems.into_iter().peekable();
-            let mut iter2 = other.elems.into_iter().peekable();
-            use std::cmp::Ordering::*;
-            while let (Some(x), Some(y)) = (iter1.peek(), iter2.peek()) {
-                match x.key.cmp(&y.key) {
-                    Less => { results.push(iter1.next().unwrap()); }
-                    Greater => { results.push(iter2.next().unwrap()); }
-                    Equal => {
-                        let mut x = iter1.next().unwrap();
-                        let y = iter2.next().unwrap();
-                        x.value = x.value.plus(y.value);
-                        results.push(x);
-                    }
+        // this is mostly copied from DataToad I think
+        let mut results: Vec<Pair<K, V>> = Vec::with_capacity(self.elems.len() + other.elems.len());
+        let mut iter1 = self.elems.into_iter().peekable();
+        let mut iter2 = other.elems.into_iter().peekable();
+        use std::cmp::Ordering::*;
+        while let (Some(x), Some(y)) = (iter1.peek(), iter2.peek()) {
+            match x.key.cmp(&y.key) {
+                Less => { results.push(iter1.next().unwrap()); }
+                Greater => { results.push(iter2.next().unwrap()); }
+                Equal => {
+                    let mut x = iter1.next().unwrap();
+                    let y = iter2.next().unwrap();
+                    x.value = x.value.plus(y.value);
+                    results.push(x);
                 }
             }
-            results.extend(iter1);
-            results.extend(iter2);
-            return Layer { elems: results }
-
-        } else {                // outer_join
-            self.elems = tuples(self.elems.as_slice(), |x| x.key)
-                .outer_join(tuples(other.elems.as_slice(), |x| x.key))
-                .map(|outer| match outer {
-                    //  ~*~=~*~  SEMIRING SEMANTICS  ~*~=~*~
-                    OuterPair::Both(x, y) => x.value.clone().plus(y.value.clone()),
-                    OuterPair::Left(x) => x.value.clone(),
-                    OuterPair::Right(y) => y.value.clone(),
-                })
-                .iter()
-                .map(|(key, value)| Pair { key, value })
-                .collect();
-            return self
         }
+        results.extend(iter1);
+        results.extend(iter2);
+        return Layer { elems: results }
     }
+
+    // fn merge_outer_join(mut self, other: Layer<Pair<K, V>>) -> Self
+    // where V: Add + Clone
+    // {
+    //     println!(" merging {} with {}", self.len(), other.len());
+    //     self.elems = tuples(self.elems.as_slice(), |x| x.key)
+    //         .outer_join(tuples(other.elems.as_slice(), |x| x.key))
+    //         .map(|outer| match outer {
+    //             //  ~*~=~*~  SEMIRING SEMANTICS  ~*~=~*~
+    //             OuterPair::Both(x, y) => x.value.clone().plus(y.value.clone()),
+    //             OuterPair::Left(x) => x.value.clone(),
+    //             OuterPair::Right(y) => y.value.clone(),
+    //         })
+    //         .iter()
+    //         .map(|(key, value)| Pair { key, value })
+    //         .collect();
+    //     return self
+    // }
 }
 
 // Log-structured merge "tree" of sorted lists.
-#[derive(Clone)]
+//
+// TODO: make this cheaply Clone-able. In particular, cloning should not require copying
+// any layer's contents; only the vector of layers should need copying. This is because we
+// don't mutate layers: we only add and merge them, and merging is not done
+// in-place/destructively.
+//
+// do we have to put Cow<> or RC<> on each layer for this?
 pub struct LSM<A> {
     // A list of layers in order, smallest first.
     // No two layers are within a power of two in size.
+    //
+    // Should we use an OuterArray here? There will be at most 48 layers. But, I doubt
+    // this would improve performance measurably.
     layers: Vec<Layer<A>>,
 }
 
@@ -162,7 +199,7 @@ impl<A> LSM<A> {
 // https://github.com/frankmcsherry/blog/blob/master/posts/2025-06-03.md#data-oriented-design--columns
 impl<K, V> LSM<Pair<K, V>> where
     K: Copy + Ord,
-    // V: Clone + Add needed for Layer::merge. We might be able to get rid of
+    // V: Clone + Add needed for Layer::merged. We might be able to get rid of
     // the Clone requirement with some careful programming.
     V: Clone + Add,
 {
@@ -184,7 +221,10 @@ impl<K, V> LSM<Pair<K, V>> where
             while self.layers.len() > pos + 1 {
                 let x = self.layers.pop().unwrap();
                 let y = self.layers.pop().unwrap();
-                self.layers.push(x.merge(y));
+                // NB. merge() gives a SMALL speedup over than merged(). trans on 100k edges:
+                // 6.3s (merge) vs 6.15s (merged) = 1.025x speedup
+                // self.layers.push(x.merge(y));
+                self.layers.push(x.merged(&y));
                 self.layers.sort_by(|x, y| y.len().cmp(&x.len()));
             }
         }
