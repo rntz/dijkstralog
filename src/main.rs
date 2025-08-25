4128b3d4cc43fa6fce03cbb6b74e7d1fd1e9d9c8
Michael Arntzenius <daekharel@gmail.com>
2025-08-25 13:53:51 +0200

    changes to src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-use dijkstralog;
+#![allow(unused_imports, unused_variables)]
+
+use dijkstralog::{
+    relationize,
+};
+use dijkstralog::macros::{
+    query,
+    rule,
+};
+
 fn main() {
     let xs: &[(u32, &str)] = &[(1, "one"), (1, "wun")];
     assert!(xs.is_sorted());
-    let _r = dijkstralog::relationize!(xs, u32, &str);
-    // let _q = dijkstralog::iter::Seek::map(dijkstralog::iter::ranges(xs, |x| x.0), |v| v);
-    println!("hello, world");
+    let _r = relationize!(xs, u32, &str);
+
+    let foo = "foo";
+    let bar = "bar";
+    let hey = "hey";
+    rule! {
+        hey() <-
+          foo("foo", 17),
+          bar("bar", xs)
+    };
 }
